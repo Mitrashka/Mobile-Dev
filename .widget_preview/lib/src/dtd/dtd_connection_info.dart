274f3deb3695b32f6c553e8e35a14fcaf31da497
Mitrashka <revivedermine086@gmail.com>
2026-09-13 14:44:57 +0900

    commit
    Создание проекта

---
+++ .widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:63976/7IYg3_4klfw=';
+const String kWidgetPreviewService =
+    'widget-preview-2172c4bf-c120-4699-90be-c808f26f9821';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-2172c4bf-c120-4699-90be-c808f26f9821';
+const String kProjectRootPath =
+    r'C:\Users\reviv\OneDrive\Desktop\Android dev\lab 1\flutter_application_1';
